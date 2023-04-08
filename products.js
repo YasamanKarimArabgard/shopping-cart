36c5e3866b708cab68943bd8186b5b006ea54381
jasminearabgard <yasaman.arabgard@outlook.com>
2023-04-09 01:46:40 +0430

    create product json array

---
+++ products.js
@@ -0,0 +1,45 @@
+export const productsData = [
+    {
+      id: 1,
+      title: "Concordia",
+      price: 10.99,
+      imageUrl: "./images/product-1.jpeg",
+    },
+    {
+      id: 2,
+      title: "Spring flower",
+      price: 14.99,
+      imageUrl: "./images/product-2.jpeg",
+    },
+    {
+      id: 3,
+      title: "Zara - seoul",
+      price: 7.99,
+      imageUrl: "./images/product-3.jpeg",
+    },
+    {
+      id: 4,
+      title: "Coco chanel",
+      price: 11.99,
+      imageUrl: "./images/product-4.jpeg",
+    },
+    {
+      id: 5,
+      title: "L'interdit - givenchy",
+      price: 4.99,
+      imageUrl: "./images/product-5.jpeg",
+    },
+    {
+      id: 6,
+      title: "London - Jo Malone",
+      price: 23.99,
+      imageUrl: "./images/product-6.jpeg",
+    },
+    {
+      id: 7,
+      title: "Elixir",
+      price: 42.99,
+      imageUrl: "./images/product-7.jpeg",
+    }
+  ];
+  
