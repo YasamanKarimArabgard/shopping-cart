d7c18ed08dea0f3002aeee4e765b093faa4378c5
jasminearabgard <yasaman.arabgard@outlook.com>
2023-04-09 03:09:58 +0430

    add dark mode

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const showBtn = document.querySelector('.show-modal');
 const backdrop = document.querySelector('.backdrop');
 const cartModal = document.querySelector('.cart');
+const darkModeBtn = document.querySelector('#dark_mode')
 
-showBtn.addEventListener('click', showModal);
+// showBtn.addEventListener('click', showModal);
+darkModeBtn.addEventListener('click', darkTheme)
 
 function showModal(){
     backdrop.style.display = 'block';
     cartModal.style.opacity = '1';
     cartModal.style.top = '20%'  
+}
+
+function darkTheme(){
+    document.body.classList.toggle('dark_mode')
 }
