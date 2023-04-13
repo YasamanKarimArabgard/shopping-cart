9beff4101726006955c77680f08559cca34c488c
jasminearabgard <yasaman.arabgard@outlook.com>
2023-04-13 22:51:37 +0430

    add new img

--- products.js
+++ products.js
@@ -1,45 +1,45 @@
 export const productsData = [
     {
       id: 1,
-      title: "Concordia",
+      title: "BOSS",
       price: 10.99,
-      imageUrl: "./images/product-1.jpeg",
+      imageUrl: "./images/product-1.jpg",
     },
     {
       id: 2,
-      title: "Spring flower",
+      title: "L'interdit - givenchy",
       price: 14.99,
-      imageUrl: "./images/product-2.jpeg",
+      imageUrl: "./images/product-2.jpg",
     },
     {
       id: 3,
       title: "Zara - seoul",
       price: 7.99,
-      imageUrl: "./images/product-3.jpeg",
+      imageUrl: "./images/product-3.jpg",
     },
     {
       id: 4,
-      title: "Coco chanel",
+      title: "SON-Do",
       price: 11.99,
-      imageUrl: "./images/product-4.jpeg",
+      imageUrl: "./images/product-4.jpg",
     },
     {
       id: 5,
-      title: "L'interdit - givenchy",
+      title: "CHANEL-N°5 Paris",
       price: 4.99,
-      imageUrl: "./images/product-5.jpeg",
+      imageUrl: "./images/product-5.jpg",
     },
     {
       id: 6,
       title: "London - Jo Malone",
       price: 23.99,
-      imageUrl: "./images/product-6.jpeg",
+      imageUrl: "./images/product-6.jpg",
     },
     {
       id: 7,
       title: "Elixir",
       price: 42.99,
-      imageUrl: "./images/product-7.jpeg",
+      imageUrl: "./images/product-7.jpg",
     }
   ];
   
